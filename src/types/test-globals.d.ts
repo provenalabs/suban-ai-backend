4f45d794b778b9634b0d7d8c3029e55359755b2a
junman140 <godswillitina@gmail.com>
2026-01-28 13:33:27 +0100

    sks

--- src/types/test-globals.d.ts
+++ src/types/test-globals.d.ts
@@ -1,9 +1,15 @@
 declare function describe(name: string, fn: () => void): void;
 declare function it(name: string, fn: () => void): void;
+declare function beforeAll(fn: () => void): void;
+declare function afterAll(fn: () => void): void;
 declare function expect(actual: any): {
     toBe(expected: any): void;
+    toBeNull(): void;
+    toContain(expected: string): void;
     not: {
         toBe(expected: any): void;
+        toBeNull(): void;
+        toContain(expected: string): void;
     };
 };
 
